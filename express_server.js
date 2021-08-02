d35f80b1ed171886d7f1c6c91006ef0eaa294430
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-08-02 14:15:05 -0400

    initialized the http server

--- express_server.js
+++ express_server.js
@@ -7,10 +7,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
