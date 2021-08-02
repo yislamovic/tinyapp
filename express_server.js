1d070a34f102accd65bb91205000693961413f58
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-08-02 19:22:07 -0400

    completed all routes with redirects

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ function generateRandomString() {
   let alphaNum = Math.random().toString(32);
   return alphaNum.split('').splice(5, 6).join('');
 }
-console.log(generateRandomString());
+// console.log(generateRandomString());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs"); //ejs template
@@ -40,11 +40,21 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.longURL] };
   res.render("urls_show", templateVars);
+  
+});
+
+app.get(`/u/:shortURL`, (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
 });
+
