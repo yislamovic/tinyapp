0b29f7e47283e6b4e2dcfe6655510753026021fe
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-08-02 17:12:38 -0400

    added generateRandomString function for the unique 6 char/num tiny url

--- express_server.js
+++ express_server.js
@@ -2,7 +2,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+function generateRandomString() {
+  let alphaNum = Math.random().toString(32);
+  return alphaNum.split('').splice(5, 6).join('');
+}
+console.log(generateRandomString());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs"); //ejs template
 
 const urlDatabase = {
@@ -27,7 +35,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
